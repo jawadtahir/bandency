27c241da77382d2cc8c90b8fa5abb981beef2d7c
Christoph Doblander <chris@degree.at>
2020-11-10 17:52:00 +0100

    added Server which serves the Challenge

--- challenger/src/main/java/de/tum/i13/Main.java
+++ challenger/src/main/java/de/tum/i13/Main.java
@@ -1,5 +1,8 @@
 package de.tum.i13;
 
+import de.tum.i13.bandency.ChallengerGrpc;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.tinylog.Logger;
 
 import java.nio.file.Path;
@@ -19,6 +22,13 @@ public class Main {
 
             Logger.info("Initializing Challenger Service");
             ChallengerServer cs = new ChallengerServer(ld);
+
+
+            Logger.info("Initializing Service");
+            Server server = ServerBuilder.forPort(8081).addService(cs).build();
+            server.start();
+            Logger.info("Serving");
+            server.awaitTermination();
         } catch (Exception ex) {
             Logger.error(ex);
         }
