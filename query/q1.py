86de20809b8ab211dc65cbb05dd3d2b0a923c2c4
Christoph Doblander <chris@degree.at>
2020-11-18 15:20:42 +0100

    added proxy

--- query/q1.py
+++ query/q1.py
@@ -14,6 +14,7 @@ class QueryOne:
 
     def run(self):
         loc = self.challengerstub.GetLocations(empty_pb2.Empty())
+
         print('got location data')
         print('QueryOne: %s' % len(loc.locations))
 
@@ -21,7 +22,7 @@ class QueryOne:
 def main():
     op = [('grpc.max_send_message_length', 100 * 1024 * 1024),
           ('grpc.max_receive_message_length', 100 * 1024 * 1024)]
-    with grpc.insecure_channel('127.0.0.1:8081', options=op) as channel:
+    with grpc.insecure_channel('challenge.msrg.in.tum.de:8081', options=op) as channel:
         stub = challenger_pb2_grpc.ChallengerStub(channel)
         q1 = QueryOne(stub)
         q1.run()
