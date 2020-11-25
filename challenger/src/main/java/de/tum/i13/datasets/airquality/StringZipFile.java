ebb3a6e6a7c52cc56c40697d41ff63ace79496c3
Christoph Doblander <chris@degree.at>
2020-11-25 22:34:35 +0100

    increased buffer size, read 10MB chunks

--- challenger/src/main/java/de/tum/i13/datasets/airquality/StringZipFile.java
+++ challenger/src/main/java/de/tum/i13/datasets/airquality/StringZipFile.java
@@ -20,7 +20,7 @@ public class StringZipFile {
         ZipEntry zipEntry = zipFile.entries().nextElement();
         InputStream stream = zipFile.getInputStream(zipEntry);
         InputStreamReader isr = new InputStreamReader(stream, StandardCharsets.UTF_8);
-        BufferedReader br = new BufferedReader(isr, 64*1000);
+        BufferedReader br = new BufferedReader(isr, 10*1024*1024);
 
         return new StringZipFileIterator(zipFile, zipEntry, stream, isr, br);
     }
