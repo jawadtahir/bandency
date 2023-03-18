10a2eac478eadd5e34281c1768c20ed0a9fa1667
Christoph Doblander <chris@degree.at>
2023-03-18 01:06:59 +0100

    add in memory data source

--- challenger/src/main/java/de/tum/i13/datasets/hdd/HddBatchIterator.java
+++ challenger/src/main/java/de/tum/i13/datasets/hdd/HddBatchIterator.java
@@ -1,11 +1,41 @@
 package de.tum.i13.datasets.hdd;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 
 import de.tum.i13.bandency.Batch;
+import de.tum.i13.datasets.cache.CloseableSource;
 
-public class HddBatchIterator {
+public class HddBatchIterator implements CloseableSource<Batch> {
+    private ArrayList<Batch> batches;
+    private int pointer;
+    private Instant stopTime;
+
     public HddBatchIterator(ArrayList<Batch> batches, Instant stopTime) {
+        this.batches = batches;
+        this.pointer = 0;
+        this.stopTime = stopTime;
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+        if (Instant.now().isAfter(stopTime)) {
+            return false;
+        } 
+        else {
+            return this.batches.size() > this.pointer;
+        }
+    }
+
+    @Override
+    public Batch nextElement() {
+        var b = this.batches.get(this.pointer);
+        ++this.pointer;
+        return b;
+    }
+
+    @Override
+    public void close() throws IOException {
     }
 }
