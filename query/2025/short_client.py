97aac8b7f170d042f877afd9946c2d4bb2880051
Jawad Tahir <jawadtahir@users.noreply.github.com>
2025-02-13 07:17:35 +0100

    changed pod failure type to pod-failure, working client and job, cleanup

--- query/2025/short_client.py
+++ query/2025/short_client.py
@@ -1,33 +1,23 @@
 import requests
+import umsgpack
 import os
 
-# PARAMS = {
-#     "apitoken": "<API TOKEN>",
-#     "test": True,
-#     "name": "<benchmark name>"
-# }
-
-# REST_SRVR="http://localhost:52923/api"
-# PARAMS = {        localhost                           localcluster
-#     "apitoken": "gfbmznvoywwogmwepbvvnbjbrxacvded", "vynyonxizjnizuphrurbuylnlxryufjc"
-#     "test": True,
-#     "name": "Test1"
-# }
-
-REST_SRVR = os.environ.get("REST_SERVER_ADDRESS", "http://localhost:52923/api") # Change the dafault to challenge2025.debs.org
+REST_SRVR =  "http://challenge2025.debs.org:52923/api"
 PARAMS = {
-    "apitoken": os.environ.get("API_TOKEN", ""),
+    "apitoken": os.environ.get("API_TOKEN"),
     "test": True,
     "name": "Test1"
 }
 
-bench_id = requests.post(REST_SRVR+"/create", json=PARAMS, headers={"Content-Type": "application/json"}).json()["bench_id"]
+bench_id = requests.post(REST_SRVR+"/create", json=PARAMS).json()
 requests.post(REST_SRVR+"/start/{}".format(bench_id))
 batch = requests.get(REST_SRVR+"/next_batch/{}".format(bench_id))
 
 while(batch.status_code == 200):
+    batch = umsgpack.unpackb(batch.content)
+    print(batch["batch_id"])
     results = {} # solution(batch)
-    requests.post(REST_SRVR+"/result/{}/{}".format(bench_id, batch.json()["batch_number"]))
+    requests.post(REST_SRVR+"/result/0/{}/{}".format(bench_id, batch["batch_id"]))
     batch = requests.get(REST_SRVR+"/next_batch/{}".format(bench_id))
 
 requests.post(REST_SRVR+"/end/{}".format(bench_id))
